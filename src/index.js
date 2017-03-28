e374c1520edc3b85a76a483c216aca44c088d72c
Dylan Vu <dylan.n.vu@gmail.com>
2017-03-28 03:05:56 -0700

    wire up middleware and hot module reloading

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import reducer from './reducers';
+import './index.css';
 import '../semantic/dist/semantic.min.css';
 
-const store = createStore(reducer)
+const loggerMiddleware = createLogger();
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware, loggerMiddleware)
+);
+
+const wrapApp = (AppComponent, reduxStore) => (
+  <BrowserRouter>
+    <Provider store={reduxStore}>
+      <AppComponent />
+    </Provider>
+  </BrowserRouter>
+);
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(wrapApp(App, store), rootEl);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(wrapApp(NextApp, store), rootEl);
+  });
+}
