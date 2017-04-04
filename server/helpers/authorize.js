3a7ea73ae701f78d453fc20d193ec1483910abb4
Dylan Vu <dylan.n.vu@gmail.com>
2017-04-03 18:25:47 -0700

    add authorize helper and validations

---
+++ server/helpers/authorize.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const boom = require('boom');
+const jwt = require('jsonwebtoken');
+
+const authorize = (req, res, next) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      return next(boom.create(401, 'Unauthorized'));
+    }
+
+    req.claim = payload;
+
+    next();
+  });
+};
+
+module.exports = authorize;
---
+++ server/validations/users.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Joi = require('joi');
+
+module.exports = {
+  register: {
+    body: {
+      firstName: Joi.string()
+        .label('First Name')
+        .required()
+        .trim(),
+      lastName: Joi.string()
+        .label('Last Name')
+        .required()
+        .trim(),
+      email: Joi.string()
+        .label('Email')
+        .required()
+        .trim()
+        .email(),
+      password: Joi.string()
+        .label('Password')
+        .required()
+        .trim()
+        .min(8),
+      passwordConfirmation: Joi.any()
+        .valid(Joi.ref('password'))
+        .required()
+        .options({
+          language: {
+            any: {
+              allowOnly: 'must match password'
+            }
+          }
+        })
+    }
+  },
+  login: {
+    body: {
+      email: Joi.string()
+        .label('Email')
+        .required()
+        .trim()
+        .email(),
+      password: Joi.string()
+        .label('Password')
+        .required()
+        .trim()
+        .alphanum()
+    }
+  }
+};
